01b37fdd21e163ae48d9c89732b578cb94cd4614
Андрей Елистратов <elistratov2001.2017@yandex.ru>
2023-10-28 19:46:24 +0000

    Corrected edits

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['51.250.16.168', '127.0.0.1', 'localhost', 'kittygramdeploy.myftp.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
